747b13aae51de96a1bf0d947c09ca5b76370969c
koreahak <gksrnrgozjxla@naver.com>
2020-04-27 15:09:58 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,13 +7,10 @@ client = commands.Bot(command_prefix = '-')
 @client.event
 async def on_ready():
 
-  
-  #await client.change_presence(status=discord.Status.online) #온라인
-  #await client.change_presence(status=discord.Status.idle) #자리비움
-  #await client.change_presence(status=discord.Status.dnd) #다른용무
-  await client.change_presence(status=discord.Status.offline) #오프라인
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.idle)
 
-  await client.change_presence(activity=discord.Game(name="게임 ㅇ하는중"))
+  await client.change_presence(activity=discord.Game(name="게임 하는중"))
   #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
